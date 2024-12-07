651a2bb7c0b093f03b86e476f45cf9c2c3a0b28c
Yasssin9 <Ahmedyassin321@gmail.com>
2024-12-07 21:25:18 +0200

    All questions

---
+++ Assignment#2/Assignment#2/pointer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class pointer
+    {
+        public string X { get; set; }
+        public string Y { get; set; }
+    }
+}
